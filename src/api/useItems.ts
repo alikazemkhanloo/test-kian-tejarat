cfe3f399860cae0f9350cedf8e922b9709f047a2
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2024-11-23 20:27:15 +0330

    fix grouping with sticky headers

--- src/api/useItems.ts
+++ src/api/useItems.ts
@@ -2,23 +2,8 @@ import { QueryFunction, useInfiniteQuery } from "@tanstack/react-query";
 import { axios_client } from "./axiosClient";
 import { per_page } from "@/constants";
 import fakeData from "./fakeData.json";
+import { Item } from "./types";
 
-type Item = {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  images: string[];
-  creationAt: string;
-  updatedAt: string;
-  category: {
-    id: number;
-    name: string;
-    image: string;
-    creationAt: string;
-    updatedAt: string;
-  };
-};
 
 const fetchItems: QueryFunction<Item[], string[], number> = async ({
   pageParam,
--- src/modules/Home/pages/Home.tsx
+++ src/modules/Home/pages/Home.tsx
@@ -1,30 +1,46 @@
+import { Item } from "@/api/types";
 import { useInfinitItems } from "@/api/useItems";
 import { useMemo } from "react";
-
+type GroupedData = { date: string; data: Item[] };
 export const Home = () => {
   const { data, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useInfinitItems();
 
-  const flatData = useMemo(() => {
-    return data?.pages?.flat(1);
+  const groupedData = useMemo(() => {
+    const result: GroupedData[] = [];
+    data?.pages.map((page) =>
+      page.map((item) => {
+        const currentGroupIndex = result.findIndex(
+          (i) => i.date === item.creationAt
+        );
+        if (currentGroupIndex >= 0) {
+          result[currentGroupIndex].data.push(item);
+        } else {
+          result.push({ date: item.creationAt, data: [item] });
+        }
+      })
+    );
+    return result
   }, [data]);
-
+  
   return (
     <div>
-      {flatData?.map((item, index) => {
+      {groupedData?.map((group) => {
         return (
-          <>
-            {item.creationAt != flatData[index - 1]?.creationAt && (
-              <div className="sticky top-0 bg-black">{item.creationAt}</div>
-            )}
-            <div>
-              {item.title}
-              <img
-                src={item.images?.[0]}
-                className="w-full h-60 object-cover"
-              />
-            </div>
-          </>
+          <div>
+            <div className="sticky top-0 bg-black">{group.date}</div>
+            {group.data.map((item) => {
+              return (
+                <div>
+                  {item.title}
+                  <img
+                    src={item.images?.[0]}
+                    className="w-full h-60 object-cover"
+                  />
+                </div>
+              );
+            })}
+          </div>
         );
       })}
       <button onClick={() => fetchNextPage()}>load more</button>
